d904126fcc7cfa40123758af79c111c74778a1d7
modsaid <mahmoud@modsaid.com>
2011-09-22 13:18:38 +0200

    adding the banner javascript file

---
+++ banner.js
@@ -0,0 +1,4 @@
+var newdiv = document.createElement('div');
+newdiv.setAttribute('style',"position:fixed; top:0; right:0; z-index:1000");
+newdiv.innerHTML = '<img src="https://github.com/modsaid/prototype-banner/raw/master/images/protype_banner.png"/>';
+document.body.appendChild(newdiv);
